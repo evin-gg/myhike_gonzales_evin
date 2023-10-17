c686c185f4ef287ff3577c90a814ba61179376db
Evin   <evingonzales2001@gmail.com>
2023-10-17 12:25:22 -0700

    can't figure out why firebase app cannot be found..

--- scripts/firebaseAPI_TEAM((.js
+++ scripts/firebaseAPI_TEAM((.js
@@ -2,13 +2,14 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
+    apiKey: "AIzaSyAoB_L8wAbYvJD-TBrjNC98ulN-3yZ0z88",
+    authDomain: "gonzales-evin-demo07.firebaseapp.com",
+    projectId: "gonzales-evin-demo07",
+    storageBucket: "gonzales-evin-demo07.appspot.com",
+    messagingSenderId: "481396946906",
+    appId: "1:481396946906:web:047e7fd3a5d606a68132e3",
+    measurementId: "G-HW2E1SCPDK"
+  };
 
 //--------------------------------------------
 // initialize the Firebase app
